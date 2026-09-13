94495f20d681914fd3897574ac743dcce87c1059
YameenRashid <yameenr7@gmail.com>
2026-09-13 18:15:33 +0100

    Add Terraform apply pipeline

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -143,3 +143,57 @@ resource "aws_iam_openid_connect_provider" "github" {
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:YameenRashid/EKS-Project-YR:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "github-actions-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ec2" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_eks" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_iam" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_s3" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_dynamodb" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
 output "external_dns_role_arn" {
   value = module.iam.external_dns_role_arn
 }
+
+output "github_actions_role_arn" {
+  value = module.iam.github_actions_role_arn
+}
